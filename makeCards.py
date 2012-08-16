cd5384ddaca17a9ea6caa5c9086423c0444d9ae1
Justin Hugon <>
2012-08-16 17:53:35 +0200

    makeCards.py now can handle multiple signal samples

--- makeCards.py
+++ makeCards.py
@@ -5,6 +5,7 @@ import ROOT as root
 from helpers import *
 import datetime
 import sys
+import copy
 
 from xsec import *
 
@@ -13,12 +14,17 @@ if scaleHiggsBy != 1.0:
   sys.exit(1)
 
 class Analysis:
-  def __init__(self,directory,signalName,backgroundNames,analysis,massRange=[123,127],histNameBase="mDiMu"):
-    self.sigFile = root.TFile(directory+signalName+".root")
-    self.name = analysis
+  def __init__(self,directory,signalNames,backgroundNames,analysis,massRange=[123,127],histNameBase="mDiMu"):
+    self.sigNames = signalNames
     self.bakNames = backgroundNames
-    self.sigHist = self.sigFile.Get(histNameBase+analysis)
-    self.sigName = signalName
+
+    self.sigFiles = []
+    self.sigHists = []
+    for name in signalNames:
+      tmpF = root.TFile(directory+name+".root")
+      tmpH = tmpF.Get(histNameBase+analysis)
+      self.sigFiles.append(tmpF)
+      self.sigHists.append(tmpH)
 
     self.bakFiles = []
     self.bakHists = []
@@ -30,7 +36,7 @@ class Analysis:
 
     effMap = {}
     xsecMap = {}
-    axis = self.sigHist.GetXaxis()
+    axis = self.sigHists[0].GetXaxis()
     lowBin = axis.FindBin(massRange[0])
     # Hack b/c root isn't giving correct bin
     if(abs(axis.GetBinLowEdge(lowBin)-massRange[0])>1e-8 and abs(axis.GetBinLowEdge(lowBin+1)-massRange[0])<1e-8):
@@ -42,27 +48,42 @@ class Analysis:
     highBin -= 1
     assert(abs(axis.GetBinUpEdge(highBin)-massRange[1])<1e-8)
 
-    countsSig = self.sigHist.Integral(lowBin,highBin)
-    effSig = countsSig/nEventsMap[signalName]
-    xsecSig = effSig*xsec[signalName]
-    self.effSig = effSig
-    self.xsecSig = xsecSig
+    self.xsecSigTotal = 0.0
+    self.xsecSigList = []
+    self.effSigList = []
+    for h,name in zip(self.sigHists,signalNames):
+      counts = h.Integral(lowBin,highBin)
+      eff = counts/nEventsMap[name]
+      xs = eff*xsec[name]
+      self.xsecSigTotal += xs
+      self.xsecSigList.append(xs)
+      self.effSigList.append(eff)
 
     self.xsecBakTotal = 0.0
     self.xsecBakList = []
     self.effBakList = []
     for h,name in zip(self.bakHists,backgroundNames):
-      countsBak = h.Integral(lowBin,highBin)
-      effBak = countsBak/nEventsMap[name]
-      xsecBak = effBak*xsec[name]
-      self.xsecBakTotal += xsecBak
-      self.xsecBakList.append(xsecBak)
-      self.effBakList.append(effBak)
+      counts = h.Integral(lowBin,highBin)
+      eff = counts/nEventsMap[name]
+      xs = eff*xsec[name]
+      self.xsecBakTotal += xs
+      self.xsecBakList.append(xs)
+      self.effBakList.append(eff)
 
-  def getSigEff(self):
-    return self.effSig
-  def getSigXSec(self):
-    return self.xsecSig
+  def getSigEff(self,name):
+    result = -1.0
+    if self.sigNames.count(name)>0:
+        i = self.sigNames.index(name)
+        result = self.effSigList[i]
+    return result
+  def getSigXSec(self,name):
+    result = -1.0
+    if self.sigNames.count(name)>0:
+        i = self.sigNames.index(name)
+        result = self.xsecSigList[i]
+    return result
+  def getSigXSecTotal(self):
+    return self.xsecSigTotal
   def getBakXSecTotal(self):
     return self.xsecBakTotal
   def getBakXSec(self,bakName):
@@ -73,18 +94,17 @@ class Analysis:
     return result
 
 class DataCardMaker:
-  def __init__(self,directory,signalAnalysisPairs,backgroundNames,massRange=[123,127],nuisanceMap=None,histNameBase="mDiMu"):
+  def __init__(self,directory,analysisNames,signalNames,backgroundNames,massRange=[123,127],nuisanceMap=None,histNameBase="mDiMu"):
   #def __init__(self,directory,signalName,backgroundNames,analysisList,massRange=[123,127]):
     channels = []
-    self.channelNames = []
+    self.channelNames = copy.deepcopy(analysisNames)
     if type(massRange[0])!=list:
-      massRange = [massRange for x in signalAnalysisPairs]
-    if len(massRange) != len(signalAnalysisPairs):
-      print("Error: DataCardMaker.__init__: massRange must either be two floats or a list of lists of the same length as signalAnalysisPairs! {0} != {1}".format(len(massRange),len(signalAnalysisPairs)))
-    for pair, mr in zip(signalAnalysisPairs,massRange):
-      tmp = Analysis(directory,pair[0],backgroundNames,pair[1],massRange=mr,histNameBase=histNameBase)
+      massRange = [massRange for x in analysisNames]
+    if len(massRange) != len(analysisNames):
+      print("Error: DataCardMaker.__init__: massRange must either be two floats or a list of lists of the same length as signalAnalysisPairs! {0} != {1}".format(len(massRange),len(analysisNames)))
+    for analysis, mr in zip(analysisNames,massRange):
+      tmp = Analysis(directory,signalNames,backgroundNames,analysis,massRange=mr,histNameBase=histNameBase)
       channels.append(tmp)
-      self.channelNames.append(pair[1])
     self.channels = channels
 
     if nuisanceMap == None:
@@ -103,14 +123,16 @@ class DataCardMaker:
         if len(name)>self.largestChannelName:
           self.largestChannelName = len(name)
     for channel in channels:
-      if len(channel.sigName)>self.largestChannelName:
-          self.largestChannelName = len(channel.sigName)
+      for name in channel.sigNames:
+        if len(name)>self.largestChannelName:
+          self.largestChannelName = len(name)
       for name in channel.bakNames:
         if len(name)>self.largestChannelName:
           self.largestChannelName = len(name)
     if self.largestChannelName < 8:
         self.largestChannelName = 8
     self.largestChannelName += 2
+    self.sigNames = signalNames
     self.bakNames = backgroundNames
 
     if self.channelNames.count("")>0:
@@ -162,25 +184,26 @@ class DataCardMaker:
     rateFormatList = []
     iParam = 0
     for channel,channelName in zip(self.channels,self.channelNames):
-        iProc = 0
-        binFormatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
-        binFormatList.append(channelName)
+        iProc = -len(channel.sigNames)+1
+        for sigName in self.sigNames:
+          binFormatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
+          binFormatList.append(channelName)
   
-        proc1FormatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
-        proc1FormatList.append(channel.sigName)
+          proc1FormatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
+          proc1FormatList.append(sigName)
   
-        proc2FormatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
-        proc2FormatList.append(iProc)
+          proc2FormatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
+          proc2FormatList.append(iProc)
   
-        expNum = channel.getSigXSec()*lumi
-        decimals = ".4f"
-        if expNum>1000.0:
+          expNum = channel.getSigXSec(sigName)*lumi
+          decimals = ".4f"
+          if expNum>1000.0:
             decimals = ".4e"
-        rateFormatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+decimals+"} "
-        rateFormatList.append(expNum)
-        iParam += 1
-        iProc += 1
+          rateFormatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+decimals+"} "
+          rateFormatList.append(expNum)
   
+          iParam += 1
+          iProc += 1
         for bakName in self.bakNames:
           binFormatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
           binFormatList.append(channelName)
@@ -217,14 +240,13 @@ class DataCardMaker:
       formatList = [nu,"lnN"]
       iParam = 2
       for channel,channelName in zip(self.channels,self.channelNames):
-          iProc = 2
-          formatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
-          value = "-"
-          if thisNu[1].count(channel.sigName)>0:
-            value = thisNu[0]+1.0
-          formatList.append(value)
-          iParam += 1
-    
+          for sigName in self.sigNames:
+            formatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
+            value = "-"
+            if thisNu[1].count(sigName)>0:
+              value = thisNu[0]+1.0
+            formatList.append(value)
+            iParam += 1
           for bakName in self.bakNames:
             formatString += "{"+str(iParam)+":^"+str(self.largestChannelName)+"} "
             value = "-"
@@ -244,89 +266,90 @@ if __name__ == "__main__":
   directory = "input/open/"
   outDir = "statsCards/"
   analysisList = [
-        ["ggHmumu125","PtL30"],
-        ["ggHmumu125","Pt30to50"],
-        ["ggHmumu125","Pt50to75"],
-        ["ggHmumu125","Pt75to125"],
-        ["ggHmumu125","Pt125"],
-        ["vbfHmumu125","VBFL"],
-        ["vbfHmumu125","VBFM"],
-        ["vbfHmumu125","VBFT"]
+        "PtL30",
+        "Pt30to50",
+        "Pt50to75",
+        "Pt75to125",
+        "Pt125",
+        "VBFL",
+        "VBFM",
+        "VBFT"
   ]
+  signalNames=["ggHmumu125","vbfHmumu125"]
   backgroundNames= ["DYJetsToLL","ttbar"]
   lumiList = [5,10,15,20,25,30,40,50,75,100,200,500,1000]
 
-  bdtAnalysisList = [["ggHmumu125","MuonOnly"],["vbfHmumu125","VBF"]]
+  bdtAnalysisList = ["MuonOnly","VBF"]
   bdtCutRangeList = [[0.05,1.0],[0.08,1.0]]
 
   ## All Combined
-  dataCard = DataCardMaker(directory,analysisList,backgroundNames)
+  dataCard = DataCardMaker(directory,analysisList,signalNames,backgroundNames)
   for i in lumiList:
     dataCard.write(outDir+"combined_"+str(i)+".txt",i)
 
   ## Muon Only combined
-  ggHAnalysisList = [x for x in analysisList if x[0]=="ggHmumu125"]
-  dataCard = DataCardMaker(directory,ggHAnalysisList,backgroundNames)
+  ggHAnalysisList = [x for x in analysisList if not re.search(r"VBF",x)]
+  dataCard = DataCardMaker(directory,ggHAnalysisList,signalNames,backgroundNames)
   for i in lumiList:
     dataCard.write(outDir+"combinedMuOnly_"+str(i)+".txt",i)
 
   ## VBF Only Combined
-  vbfHAnalysisList =  [x for x in analysisList if x[0]=="vbfHmumu125"]
-  dataCard = DataCardMaker(directory,vbfHAnalysisList,backgroundNames)
+  vbfHAnalysisList =  [x for x in analysisList if re.search(r"VBF",x)]
+  dataCard = DataCardMaker(directory,vbfHAnalysisList,signalNames,backgroundNames)
   for i in lumiList:
     dataCard.write(outDir+"combinedVBFOnly_"+str(i)+".txt",i)
 
   ## Each Individual
   for i in analysisList:
-    title = i[1]
+    title = i
     if title=="":
         title="Inc"
-    dataCard = DataCardMaker(directory,[i],backgroundNames)
+    dataCard = DataCardMaker(directory,[i],signalNames,backgroundNames)
     for j in lumiList:
       dataCard.write(outDir+title+"_"+str(j)+".txt",j)
 
   ## Mass cut window optimization
   for i in range(0,50):
     amount = 0.25+i*0.25
-    dataCard = DataCardMaker(directory,analysisList,backgroundNames,massRange=[125.0-amount,125.0+amount])
+    dataCard = DataCardMaker(directory,analysisList,signalNames,backgroundNames,massRange=[125.0-amount,125.0+amount])
     title = "PMcombinedPM"+str(amount)
     dataCard.write(outDir+title+"_"+str(20)+".txt",20)
 
-    dataCard = DataCardMaker(directory,[["ggHmumu125","PtL30"]],backgroundNames,massRange=[125.0-amount,125.0+amount])
+    dataCard = DataCardMaker(directory,["PtL30"],signalNames,backgroundNames,massRange=[125.0-amount,125.0+amount])
     title = "PMPtL30PM"+str(amount)
     dataCard.write(outDir+title+"_"+str(20)+".txt",20)
 
-    dataCard = DataCardMaker(directory,[["ggHmumu125","Pt125"]],backgroundNames,massRange=[125.0-amount,125.0+amount])
+    dataCard = DataCardMaker(directory,["Pt125"],signalNames,backgroundNames,massRange=[125.0-amount,125.0+amount])
     title = "PMPt125PM"+str(amount)
     dataCard.write(outDir+title+"_"+str(20)+".txt",20)
 
-    dataCard = DataCardMaker(directory,[["vbfHmumu125","VBFL"]],backgroundNames,massRange=[125.0-amount,125.0+amount])
+    dataCard = DataCardMaker(directory,["VBFL"],signalNames,backgroundNames,massRange=[125.0-amount,125.0+amount])
     title = "PMVBFLPM"+str(amount)
     dataCard.write(outDir+title+"_"+str(20)+".txt",20)
 
-    dataCard = DataCardMaker(directory,[["vbfHmumu125","VBFT"]],backgroundNames,massRange=[125.0-amount,125.0+amount])
+    dataCard = DataCardMaker(directory,["VBFT"],signalNames,backgroundNames,massRange=[125.0-amount,125.0+amount])
     title = "PMVBFTPM"+str(amount)
     dataCard.write(outDir+title+"_"+str(20)+".txt",20)
 
   ## BDT Combination
-  dataCard = DataCardMaker(directory,bdtAnalysisList,backgroundNames,massRange=bdtCutRangeList,histNameBase="BDTHist")
+  dataCard = DataCardMaker(directory,bdtAnalysisList,signalNames,backgroundNames,massRange=bdtCutRangeList,histNameBase="BDTHist")
   for i in lumiList:
     dataCard.write(outDir+"BDTCombination"+"_"+str(i)+".txt",i)
 
   ## BDT Individual
   for ana,cuts in zip(bdtAnalysisList,bdtCutRangeList):
-    dataCard = DataCardMaker(directory,[ana],backgroundNames,massRange=cuts,histNameBase="BDTHist")
+    dataCard = DataCardMaker(directory,[ana],signalNames,backgroundNames,massRange=cuts,histNameBase="BDTHist")
     for i in lumiList:
-      dataCard.write(outDir+"BDT"+ana[1]+"_"+str(i)+".txt",i)
+      dataCard.write(outDir+"BDT"+ana+"_"+str(i)+".txt",i)
 
   ## BDT Cut Optimization
   for i in range(0,40):
     amount = 0.2-i*0.01
-    dataCard = DataCardMaker(directory,[["ggHmumu125","MuonOnly"]],backgroundNames,massRange=[amount,1.0],histNameBase="BDTHist")
+    dataCard = DataCardMaker(directory,["MuonOnly"],signalNames,backgroundNames,massRange=[amount,1.0],histNameBase="BDTHist")
     title = "BDTMuBDT"+str(amount)
     dataCard.write(outDir+title+"_"+str(20)+".txt",20)
 
-    dataCard = DataCardMaker(directory,[["vbfHmumu125","VBF"]],backgroundNames,massRange=[amount,1.0],histNameBase="BDTHist")
+    dataCard = DataCardMaker(directory,["VBF"],signalNames,backgroundNames,massRange=[amount,1.0],histNameBase="BDTHist")
     title = "BDTVBFBDT"+str(amount)
     dataCard.write(outDir+title+"_"+str(20)+".txt",20)
 
