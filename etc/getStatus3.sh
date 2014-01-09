52a316e033880dee50094c3e9bb6761fb2a3294d
Justin <jhugon@cern.fake>
2014-01-08 20:50:50 -0500

    getStatus3.sh works on HPC

--- etc/getStatus3.sh
+++ etc/getStatus3.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-njobs=`bjobs | wc -l`
-njobsRunning=`bjobs | grep "RUN" | wc -l`
-((njobs -= 1))
+hn=$(hostname)
+un=$(whoami)
+if [[ "$hn" == *cern* ]]; then
+  njobs=`bjobs | wc -l`
+  njobsRunning=`bjobs | grep "RUN" | wc -l`
+  ((njobs -= 1))
+else
+  rawJobOut=$(qstat -u $un -t)
+  njobsQ=$(echo "$rawJobOut" | grep '^[0-9].* Q ' | wc -l)
+  njobsRunning=$(echo "$rawJobOut" | grep '^[0-9].* R ' | wc -l)
+  njobs=""
+  ((njobs = njobsQ + njobsRunning))
+fi
 echo "N jobs:             "$njobs
 echo "N jobs Running:     "$njobsRunning
 echo "Running Percentage: "$(python -c "print float($njobsRunning)/$njobs")
