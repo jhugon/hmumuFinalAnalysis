48757dafd70b3d5626491f8af8e0978c90a25888
Justin <jhugon@cern.fake>
2014-02-13 18:12:40 -0500

    biasEnvelope.py: also prints relative bias table now

--- singleUseScripts/biasEnvelope.py
+++ singleUseScripts/biasEnvelope.py
@@ -10,6 +10,10 @@ import glob
 import re
 from numpy import mean,median
 
+errorsDictFile = open("pklfiles/oneSig.pkl")
+ERRORSDICT = cPickle.load(errorsDictFile)
+errorsDictFile.close()
+
 TITLEMAP = {
   "Jets01PassPtG10BB": "0,1-Jet Tight BB",
   "Jets01PassPtG10BO": "0,1-Jet Tight BO",
@@ -71,7 +75,7 @@ PDFTITLEMAP = PdfTitleMap({
     "VoigtPExpMm2":"Voigtian+#frac{Exp}{m^{2}}",
 })
 
-def biasEnvelope(useRefFuncs):
+def biasEnvelope(useRefFuncs,relativeForMH=125):
   print "+++++++++++++++++++++++++++++++++++++++++++++"
   print "+++++++++++++++++++++++++++++++++++++++++++++"
   print "+++++++++++++++++++++++++++++++++++++++++++++"
@@ -169,30 +173,43 @@ def biasEnvelope(useRefFuncs):
   plainCompareTable += "\n\n"
   latexCompareTable = "%%%%%%%%%%%%%%%%%%%%%%%%%\n%%%  Max Bias Final Table (Nevts)\n\n"
   latexCompareTable += r"\begin{tabular}{|l|"+"r|"*len(energies)+"} \\hline \n"
+  relativeCompareTable = "%%%%%%%%%%%%%%%%%%%%%%%%%\n%%%  Max Bias Final Table (Nevts/deltaNevts) deltaNevts for mH={0}\n\n".format(relativeForMH)
+  relativeCompareTable += r"\begin{tabular}{|l|"+"r|"*len(energies)+"} \\hline \n"
   plainCompareTable += "{0:20}".format("Energy:")
   latexCompareTable += "{0}".format("Energy")
+  relativeCompareTable += "{0}".format("Energy")
   for energy in energies:
     plainCompareTable += "{0:^15}".format(energy)
     latexCompareTable += " & {0:}".format(energy)
+    relativeCompareTable += " & {0:}".format(energy)
   plainCompareTable += "\n"
   latexCompareTable += "\\\\ \\hline \\hline \n"
+  relativeCompareTable += "\\\\ \\hline \\hline \n"
   for cat in sortCatNames(list(categorySet)):
     plainCompareTable += "{0:20}".format(cat)
     latexCompareTable += "{0:}".format(TITLEMAP[cat])
+    relativeCompareTable += "{0:}".format(TITLEMAP[cat])
     for energy in energies:
       plainCompareTable += "{0:^15.1f}".format(finalDict[energy][cat])
       latexCompareTable += " & {0:.1f}".format(finalDict[energy][cat])
+      relativeCompareTable += " & {0:.1f}".format(finalDict[energy][cat]/ERRORSDICT[energy][cat][relativeForMH])
     plainCompareTable += "\n"
     latexCompareTable += " \\\\ \\hline \n"
+    relativeCompareTable += " \\\\ \\hline \n"
   plainCompareTable += "\n\n"
   latexCompareTable += "\\end{tabular}\n"
+  relativeCompareTable += "\\end{tabular}\n"
   latexCompareTable += "\\\\ Included Reference Functions: "
+  relativeCompareTable += "\\\\ Included Reference Functions: "
   for refName in refNameList:
     latexCompareTable += PDFTITLEMAP[refName]+", "
+    relativeCompareTable += PDFTITLEMAP[refName]+", "
   latexCompareTable = latexCompareTable[:-2] + "\n\n"
+  relativeCompareTable = relativeCompareTable[:-2] + "\n\n"
       
   print latexCompareTable
   print plainCompareTable
+  print relativeCompareTable
     
   dictName = "BakParameterizationUncDict"
   dictCompareTable = "#########################\n###  Max Bias Final Dict (Nevts)\n\n"
