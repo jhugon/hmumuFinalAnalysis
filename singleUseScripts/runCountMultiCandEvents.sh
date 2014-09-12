81c98ae7be8b6be7eb4e6b8318e06e9dbfd685ce
Justin <jhugon@cern.fake>
2014-09-12 13:53:05 -0400

    updated files to 100k files for runCountMultiCandEvents.sh
      added unmodified not updated calcMultiMuons.py

--- singleUseScripts/runCountMultiCandEvents.sh
+++ singleUseScripts/runCountMultiCandEvents.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-root -b -x -q countMultiCandEvents.C+\(\"/raid/raid7/jhugon/hmmNtuples/stage2/ggHmumu125_8TeV.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/raid/raid7/jhugon/hmmNtuples/stage2/vbfHmumu125_8TeV.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/raid/raid7/jhugon/hmmNtuples/stage2/wHmumu125_8TeV.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/raid/raid7/jhugon/hmmNtuples/stage2/zHmumu125_8TeV.root\"\)
+inputDir=/raid/raid9/jhugon/hmmNtuples
 
-root -b -x -q countMultiCandEvents.C+\(\"/cms/data/store/user/jhugon/hmumu/stage2/DYJetsToLL_8TeV.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/cms/data/store/user/jhugon/hmumu/stage2/ttbar_8TeV.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/cms/data/store/user/jhugon/hmumu/stage2/WW_8TeV.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/cms/data/store/user/jhugon/hmumu/stage2/WZ_8TeV.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/cms/data/store/user/jhugon/hmumu/stage2/ZZ_8TeV.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/ggHmumu125_8TeV_all100kEventSamples.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/vbfHmumu125_8TeV_all100kEventSamples.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/wHmumu125_8TeV_official100kEventSamples.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/zHmumu125_8TeV_official100kEventSamples.root\"\)
 
-root -b -x -q countMultiCandEvents.C+\(\"/cms/data/store/user/jhugon/hmumu/stage2/SingleMuRun2012Av1-22Jan2013.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/cms/data/store/user/jhugon/hmumu/stage2/SingleMuRun2012Bv1-22Jan2013.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/cms/data/store/user/jhugon/hmumu/stage2/SingleMuRun2012Cv1-22Jan2013.root\"\)
-root -b -x -q countMultiCandEvents.C+\(\"/cms/data/store/user/jhugon/hmumu/stage2/SingleMuRun2012Dv1-22Jan2013.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/DYJetsToLL_8TeV.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/ttbar_8TeV.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/WW_8TeV.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/WZ_8TeV.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/ZZ_8TeV.root\"\)
+
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/SingleMuRun2012Av1-22Jan2013.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/SingleMuRun2012Bv1-22Jan2013.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/SingleMuRun2012Cv1-22Jan2013.root\"\)
+root -b -x -q countMultiCandEvents.C+\(\"$inputDir/stage2/SingleMuRun2012Dv1-22Jan2013.root\"\)
