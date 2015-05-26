acc4a23bf61d37b80267d7d764430793a23ae2a7
Justin <jhugon@cern.fake>
2015-05-26 17:34:01 -0400

    bkgFuncCheckTTFraction: now plot fit func

--- singleUseScripts/bkgFuncCheckTTFraction.py
+++ singleUseScripts/bkgFuncCheckTTFraction.py
@@ -3,6 +3,7 @@
 import singleHelpers
 from helpers import *
 from xsec import *
+from makeCards import makePDFBak
 
 def getHistogram(directory,sample,energy,catname,cutString,binSize=0.5,aroundZ=False):
     fullCutString = treeCut(catname,cutString,eventWeights=True,muonRequirements=True,KDString=None)
@@ -27,6 +28,33 @@ def getHistogram(directory,sample,energy,catname,cutString,binSize=0.5,aroundZ=F
     tmpTree.Draw(drawString,fullCutString)
     tmpHist.SetDirectory(0)
     return tmpHist
+
+def getYMaxAuto(plotHist,logy=False):
+  yTopOfFrame = 0.
+  frameSF = 1.2
+  frameRightSideSF = 1.5
+  if logy:
+    frameSF = 1.5
+    frameRightSideSF = 5.
+
+  nBins = plotHist.GetNbinsX()
+  for iBin in range(1,nBins+1):
+    yMaxThisBin = plotHist.GetBinContent(iBin)+plotHist.GetBinError(iBin)
+    if logy:
+      yTopOfFrameTry = yMaxThisBin**frameSF
+      yTopOfFrame = max(yTopOfFrame,yTopOfFrameTry)
+    else:
+      yTopOfFrameTry = frameSF*yMaxThisBin
+      yTopOfFrame = max(yTopOfFrame,yTopOfFrameTry)
+    
+  return yTopOfFrame
+
+def setupHistogram(hist,sample,energy):
+  sample_fullname = sample+"_"+energy
+  scaleFactor = xsec[sample_fullname]*1000.0*lumiDict[energy]/nEventsMap[sample_fullname]*efficiencyMap[energy]*mcPlotScaleFactorMap[energy]
+  hist.Scale(scaleFactor)
+  hist.SetFillColor(colors[sample])
+  hist.SetLineColor(colors[sample])
 
 if __name__ == "__main__":
   root.gROOT.SetBatch(True)
@@ -59,31 +87,74 @@ if __name__ == "__main__":
   #categories += [["Jet2CutsFailVBFGF","!(deltaEtaJets>3.5 && dijetMass>650.) && !(dijetMass>250. && dimuonPt>50.)"+jet2PtCuts]]
   
   canvas = root.TCanvas("canvas")
+  minMass = 110.
+  maxMass = 160.
+  dimuonMass = root.RooRealVar("dimuonMass","dimuonMass",minMass,maxMass)
+  controlRegionVeryLow=[60,110]
+  controlRegionLow=[110,120]
+  controlRegionHigh=[130,160]
+  SIGNALFIT = [110.,140.]
+  dimuonMass.setRange("exprange",120.,controlRegionHigh[1])
+  dimuonMass.setRange("whole",controlRegionLow[0],controlRegionHigh[1])
+  dimuonMass.setRange("low",controlRegionLow[0],controlRegionLow[1])
+  dimuonMass.setRange("high",controlRegionHigh[0],controlRegionHigh[1])
+  dimuonMass.setRange("signal",controlRegionLow[1],controlRegionHigh[0])
+  dimuonMass.setRange("signalfit",SIGNALFIT[0],SIGNALFIT[1])
+  minMassZ = 88.
+  maxMassZ = 94.
+  dimuonMassZ = root.RooRealVar("dimuonMass","dimuonMass",minMassZ,maxMassZ)
   for energy in periods:
     for category in categories:
       binSize = 2.0
       hists = []
       hstack = root.THStack()
+      hsum = None
+      zhists = []
+      zhsum = None
       for sample in ["ttbar","DYJetsToLL"]:
         hist = getHistogram(indir,sample,energy,category[0],category[1],binSize=binSize)
-        sample_fullname = sample+"_"+energy
-        scaleFactor = xsec[sample_fullname]*1000.0*lumiDict[energy]/nEventsMap[sample_fullname]*efficiencyMap[energy]*mcPlotScaleFactorMap[energy]
-        hist.Scale(scaleFactor)
-        hist.SetFillColor(colors[sample])
-        hist.SetLineColor(colors[sample])
+        setupHistogram(hist,sample,energy)
         hstack.Add(hist)
         hists.append(hist)
+        if hsum == None:
+          hsum = hist.Clone(energy+category[0]+"sumHist")
+        else:
+          hsum.Add(hist)
+        # now for zhist
+        zhist = getHistogram(indir,sample,energy,category[0],category[1],binSize=binSize,aroundZ=True)
+        setupHistogram(zhist,sample,energy)
+        zhists.append(zhist)
+        if zhsum == None:
+          zhsum = zhist.Clone(energy+category[0]+"zsumHist")
+        else:
+          zhsum.Add(hist)
+
+      workspace = root.RooWorkspace(energy+category[0]+"workspace")
+      wImport = getattr(workspace,"import")
+      rooHistName = "bkg"
+      zrooHistName = rooHistName+"Z"
+      rooHist = root.RooDataHist(rooHistName,rooHistName,root.RooArgList(dimuonMass),hsum)
+      zrooHist = root.RooDataHist(zrooHistName,zrooHistName,root.RooArgList(dimuonMassZ),zhsum)
+      makePDFBak(category[0],rooHist,dimuonMass,110,160,wImport,dimuonMassZ,zrooHist)
+      pdf = workspace.pdf("bak")
+
       canvas.Clear()
-      ymax = 2000
+      ymax = getYMaxAuto(hsum)
       axisHist = root.TH2F("axisHist"+energy+category[0],"",1,110,160,1,0,ymax)
       setHistTitles(axisHist,"m_{#mu#mu} [GeV]","Entries/{0:0.1f} GeV".format(binSize))
       axisHist.GetXaxis().CenterTitle(True)
       axisHist.Draw()
       hstack.Draw("same")
+
+      frame = dimuonMass.frame()
+      rooHist.plotOn(frame)
+      pdf.plotOn(frame,root.RooFit.LineColor(1))
+      frame.Draw("same")
+
       canvas.RedrawAxis()
       energyStr = energy
       if re.search(r"[\d]TeV",energyStr):
         energyStr = energyStr.replace("TeV"," TeV")
       lumiStr = "{0:.1f} fb^{{-1}} ({1})".format(lumiDict[energy],energyStr)
-      drawStandardCaptions(canvas,lumiStr,TITLEMAP[category[0]],preliminaryString="")
+      drawStandardCaptions(canvas,lumiStr,TITLEMAP[category[0]],preliminaryString="Simulated Background Data")
       canvas.SaveAs("test.png")
